706e9312cb7ad1bf796232cee856bd0238c0e8d8
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-09 18:27:35 +0300

    Connect name entered by user to nationality api

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('connected')
+// const inputName = document.querySelector('[name="name123"]')
+// const form = document.querySelector('[name="form"]')
+// console.log(inputName);
+// console.log(form);
+
+// console.log(inputName.value);
+const userInput = document.querySelector('#userName')
+const submit = document.querySelector('button')
+console.log(userInput, submit)
+submit.addEventListener('click',()=>{
+    console.log(userInput.value);
+    let link = 'https://api.nationalize.io?name='+userInput.value
+    console.log(link)
+})
