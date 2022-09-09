8b15080a1fb7ddd171db798e52411ba5e2607418
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-09 23:53:33 +0300

    Add nationality api

--- app.js
+++ app.js
@@ -1,34 +1,51 @@
-console.log('connected')
-// const inputName = document.querySelector('[name="name123"]')
-// const form = document.querySelector('[name="form"]')
-// console.log(inputName);
-// console.log(form);
+const userInput = document.querySelector('#userName');
+const submit = document.querySelector('button');
+const dogImage = document.querySelector('.dog-image');
+const list = document.querySelector('.list')
+console.log(userInput, submit);
 
-// console.log(inputName.value);
-const userInput = document.querySelector('#userName')
-const submit = document.querySelector('button')
-const dogImage = document.querySelector('.dog-image')
-let userName = ''
-console.log(userInput, submit)
-// submit.addEventListener('click',()=>{
-//     console.log(userInput.value);
-//     let link = 'https://api.nationalize.io?name='+userInput.value
-//     console.log(link.body.country)
-// })
-userName = submit.addEventListener('click',()=>{
-    name1 = userInput.value
-    return name1
+
+
+submit.addEventListener('click',()=>{
+    console.log(userInput.value);
+    let link = 'https://api.nationalize.io?name='+userInput.value
+    fetch(link)
+        .then(res=>{
+            return res.json()
+        })
+        .then(data=>{
+            console.log(data.country)
+            let i = 0;
+            let countries = data.country;
+            for(country of countries){
+                let listItem = document.createElement('li')
+                let result = document.createElement('b');
+                countryCode = country.country_id
+                countryProb = country.probability
+                console.log(countryCode, countryProb)
+                result.innerText= `${countryCode}, ${countryProb}`
+                // list.appendChild(listItem)
+                listItem.append(result)
+                list.append(listItem)
+                // console.log(i)
+                // i++
+            }
+        })
+        .catch(e=>{
+            console.log("Error", e);
+        })
 })
 
+// Dog image is done
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((res) => {
-    console.log('Response',res);
+    // console.log('Response',res);
     return res.json();
   })
   .then((data) => {
-    console.log(data);
+    // console.log(data);
     dogImage.src = `${data.message}`
   })
   .catch((e) => {
-    console.log("Eroor", e);
+    console.log("Error", e);
   });
