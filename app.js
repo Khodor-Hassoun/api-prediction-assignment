b391cdf57309942a8e1444763ca6ffedf7bc75ad
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-09 22:37:19 +0300

    Connect dog api to the html via DOM

--- app.js
+++ app.js
@@ -7,9 +7,28 @@ console.log('connected')
 // console.log(inputName.value);
 const userInput = document.querySelector('#userName')
 const submit = document.querySelector('button')
+const dogImage = document.querySelector('.dog-image')
+let userName = ''
 console.log(userInput, submit)
-submit.addEventListener('click',()=>{
-    console.log(userInput.value);
-    let link = 'https://api.nationalize.io?name='+userInput.value
-    console.log(link)
+// submit.addEventListener('click',()=>{
+//     console.log(userInput.value);
+//     let link = 'https://api.nationalize.io?name='+userInput.value
+//     console.log(link.body.country)
+// })
+userName = submit.addEventListener('click',()=>{
+    name1 = userInput.value
+    return name1
 })
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((res) => {
+    console.log('Response',res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    dogImage.src = `${data.message}`
+  })
+  .catch((e) => {
+    console.log("Eroor", e);
+  });
